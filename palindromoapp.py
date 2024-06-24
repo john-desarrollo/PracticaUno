442ad3d232056f019622dc7b57c0aa040668abf3
john-desarrollo <95327960+john-desarrollo@users.noreply.github.com>
2024-06-23 22:29:25 -0500

    Update palindromoapp.py

--- palindromoapp.py
+++ palindromoapp.py
@@ -1,17 +1,11 @@
 def es_palindromo(palabra):
-
     palabra = palabra.lower()  # Convertimos la palabra a minúsculas para evitar problemas de mayúsculas
-
     return palabra == palabra[::-1]  # Comparamos la palabra con su reverso
 
-
-palabra_usuario = input("Ingrese una palabra: ")
-
-
-if es_palindromo(palabra_usuario):
-
-    print("La palabra es un palíndromo!")
-
-else:
-
-    print("La palabra no es un palíndromo.")
+if __name__ == "__main__":
+    palabra_usuario = input("Ingrese una palabra: ")
+    
+    if es_palindromo(palabra_usuario):
+        print("La palabra es un palíndromo!")
+    else:
+        print("La palabra no es un palíndromo.")
