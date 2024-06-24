c12e6bbfa4ae453ce5a155e5e265f46a91d255c3
john-desarrollo <95327960+john-desarrollo@users.noreply.github.com>
2024-06-23 22:31:00 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ def test_palindromo():
     assert es_palindromo("radar") == True
     assert es_palindromo("python") == False
     assert es_palindromo("Ana") == True
+    assert es_palindromo("Nivel") == True
+    assert es_palindromo("Jenkins") == False
 
 if __name__ == "__main__":
     pytest.main()
