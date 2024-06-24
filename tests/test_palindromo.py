a3711e993b3c098b9711f286d2325bd3271ad45a
john-desarrollo <jhonndiaz34@gmail.com>
2024-06-23 23:19:43 -0500

    Actualizacion de configuracion

--- tests/test_palindromo.py
+++ tests/test_palindromo.py
@@ -1,12 +1,12 @@
-import pytest
+import sys
+import os
+
+# Añadir el directorio raíz del proyecto al PATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from palindromo import es_palindromo
 
 def test_palindromo():
     assert es_palindromo("radar") == True
-    assert es_palindromo("python") == False
+    assert es_palindromo("hello") == False
     assert es_palindromo("Ana") == True
-    assert es_palindromo("Nivel") == True
-    assert es_palindromo("Jenkins") == False
-
-if __name__ == "__main__":
-    pytest.main()
