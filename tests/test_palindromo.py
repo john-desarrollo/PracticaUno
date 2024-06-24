34a3ad677754b3af5c1420c16c6407059b226ef5
john-desarrollo <95327960+john-desarrollo@users.noreply.github.com>
2024-06-23 23:30:30 -0500

    Delete tests directory

--- tests/test_palindromo.py
+++
@@ -1,6 +0,0 @@
-from palindromo import es_palindromo
-
-def test_palindromo():
-    assert es_palindromo("radar") == True
-    assert es_palindromo("hello") == False
-    assert es_palindromo("Ana") == True
